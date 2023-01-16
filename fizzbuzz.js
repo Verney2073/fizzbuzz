89eef97f0a6dc25dd152aeb7272550681e210b94
James Tribe <jjtribe93@gmail.com>
2023-01-16 16:14:06 +0000

    shorthand return val

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,21 +2,22 @@
 //console.log(2);
 
 function fizzBuzz () {
-    for ( let i = 1; i <= 100; i++){
+    for ( let i = 1; i <= 105; i++){
         let fizzyAnswer = "";
         let defAnswer = i;
         let fizzy = false;
         if ( i  % 3 == 0 ){ 
             fizzyAnswer += "fizz";
+            fizzy = true;
         } 
         if (i % 5 == 0 ) {
             fizzyAnswer += "buzz"; 
-            fizzy == true;}  
+            fizzy = true;}  
         if (i % 7 == 0) {
             fizzyAnswer += "bang";
-            fizzy == true;} 
-        return console.log(fizzy ? fizzyAnswer: defAnswer);
-
+            fizzy = true;} 
+        console.log(fizzy? fizzyAnswer: defAnswer);
+        }
     };
 }
 
