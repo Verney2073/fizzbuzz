269c52ee8ca8c9ae30bcfcf922b6eeaf2c2f3646
James Tribe <jjtribe93@gmail.com>
2023-01-18 14:01:27 +0000

    typo fix

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,25 +1,26 @@
 //console.log(1);
 //console.log(2);
 
-function fizzBuzz () {
-    for ( let i = 1; i <= 105; i++){
+function fizzBuzz() {
+    for (let i = 1; i <= 105; i++) {
         let fizzyAnswer = "";
         let defAnswer = i;
         let fizzy = false;
-        if ( i  % 3 == 0 ){ 
+        if (i % 3 == 0) {
             fizzyAnswer += "fizz";
             fizzy = true;
-        } 
-        if (i % 5 == 0 ) {
-            fizzyAnswer += "buzz"; 
-            fizzy = true;}  
+        }
+        if (i % 5 == 0) {
+            fizzyAnswer += "buzz";
+            fizzy = true;
+        }
         if (i % 7 == 0) {
             fizzyAnswer += "bang";
-            fizzy = true;} 
-        console.log(fizzy? fizzyAnswer: defAnswer);
+            fizzy = true;
         }
-    };
-}
+        console.log(fizzy ? fizzyAnswer : defAnswer);
+    }
+};
 
 fizzBuzz();
 
